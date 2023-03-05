ad1b4427777f1e5eaecdb0cf8623fee4eec5a270
Manuel <Manuel@DESKTOP-6EFL5S5>
2023-03-05 15:22:27 -0300

    [ADD] Commit inicial

---
+++ src/test/java/com/mgdev/SistemaRegistroPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRegistroPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
